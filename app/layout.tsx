9b0a2a597d2b9aa4430acd348f3384ab898432d9
Le Minh Tri <minhtri0795@gmail.com>
2022-11-30 21:45:13 +0700

    :mag: add meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html className={LibreFranklin.className} lang="en">
+      <head>
+        <title>Home - Comming soon</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </head>
       <body>
         <header className="fixed w-full text-center text-xl uppercase font-semibold mt-3">
           @minhtri0795
