cc44efeec22d34c88fedb6fdd79f2515192a8d18
Le Minh Tri <minhtri0795@gmail.com>
2022-12-03 21:59:37 +0700

    ✨ setting up page layout

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => (
+  <footer className="text-sm  px-4 pt-10 pb-12 sm:pt-14 sm:pb-16 border-t border-gray-500 lg:px-0">
+    <div className="max-w-xl mx-auto">
+      <p className="font-semibold ">About this place</p>
+      <p className="mt-2">Welcome to my digital garden 👋🏻</p>
+      <p className="mt-1">
+        I'm Tri, a web developer. On this site, I occasionally write programming
+        tutorials and lessons I learned from few year working.
+      </p>
+      <nav className="mt-6">
+        <p className="mt-2">Everywhere I'm online, I'm @minhtri0795.</p>
+        <div className="mt-4 flex flex-row space-x-5"></div>
+      </nav>
+    </div>
+  </footer>
+);
+
+export default Footer;
