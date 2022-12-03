ba02ef6b53235e8d8f1902a2d6e4b69fe820199f
Le Minh Tri <minhtri0795@gmail.com>
2022-12-03 21:56:27 +0700

    🎨 global styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: { 950: "#111" },
+        green: { 250: "#88fdc5" },
+        indigo: { 550: "#513eea" },
+        magenta: "#ff0090",
+      },
+    },
   },
   plugins: [],
 };
