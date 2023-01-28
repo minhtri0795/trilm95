7b8c78ef2078a45195e067d57b1d78017bc387c3
Le Minh Tri <minhtri0795@gmail.com>
2023-01-28 19:24:20 +0700

    🎨 dark theme default

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false}>
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
       {children}
     </ThemeProvider>
   );
