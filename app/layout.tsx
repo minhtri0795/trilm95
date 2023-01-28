18bcf3a0df619a156274eaa4ad84a57d6d460c8d
Le Minh Tri <minhtri0795@gmail.com>
2023-01-28 12:32:13 +0700

    🎨 adjust layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,7 @@ export default function RootLayout({
       <body className={InterFont.className}>
         <Providers>
           <Header />
-          <main className="flex flex-col justify-center px-8 ">
-            <div className="flex flex-col justify-center items-start max-w-2xl lg:max-w-3xl mx-auto pb-16">
-              {children}
-            </div>
-          </main>
+          <main className="flex flex-col justify-center px-8 ">{children}</main>
           <Footer />
         </Providers>
       </body>
