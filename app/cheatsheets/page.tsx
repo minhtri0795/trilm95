5138819ec9457c252548fac9e0221f192f8a976b
Le Minh Tri <minhtri0795@gmail.com>
2023-01-29 16:20:55 +0700

    ✏️ fix typo

--- app/cheatsheets/page.tsx
+++ app/cheatsheets/page.tsx
@@ -3,7 +3,7 @@ function Cheatsheets() {
   return (
     <div className="flex flex-col items-start justify-center max-w-2xl lg:max-w-3xl w-full mx-auto mb-16">
       <Headings.H1>Cheat sheets</Headings.H1>
-      <Headings.H2>Commng soon...</Headings.H2>
+      <Headings.H2>Comming soon...</Headings.H2>
     </div>
   );
 }
