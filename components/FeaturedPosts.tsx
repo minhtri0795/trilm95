a6b3a8829b571334ce584fe863c1472f0f4ab16f
Le Minh Tri <minhtri0795@gmail.com>
2023-03-27 21:34:31 +0700

    ✨ updated article

--- components/FeaturedPosts.tsx
+++ components/FeaturedPosts.tsx
@@ -7,22 +7,25 @@ function FeaturedPosts() {
     <div className="">
       <Headings.H3>Featured Posts</Headings.H3>
       <div className="flex gap-6 flex-col md:flex-row">
-        {allPosts.map((post) => {
-          return (
-            <Link
-              className="transform hover:scale-[1.01] transition-all rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1 from-[#D8B4FE] to-[#818CF8]"
-              href={post.url}
-              key={post.title}
-            >
-              <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
-                <div className="flex flex-col md:flex-row justify-between">
-                  <h4 className="text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full text-gray-900 dark:text-gray-100 tracking-tight">
-                    {post.title}
-                  </h4>
+        {allPosts.flatMap((post) => {
+          if (post.featuredPost) {
+            return (
+              <Link
+                className="transform hover:scale-[1.01] transition-all rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1 from-[#D8B4FE] to-[#818CF8]"
+                href={post.url}
+                key={post.title}
+              >
+                <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
+                  <div className="flex flex-col md:flex-row justify-between">
+                    <h4 className="text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full text-gray-900 dark:text-gray-100 tracking-tight">
+                      {post.title}
+                    </h4>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
+              </Link>
+            );
+          }
+          return []
         })}
       </div>
       <Link
